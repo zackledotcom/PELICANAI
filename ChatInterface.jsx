deb231f0772fc284ec3f84eb12b1723910ae0dce
zackledotcom <zacharylonfobardi@gmail.com>
2025-05-18 01:55:24 -0500

    Updates from 5-18-2025

--- ChatInterface.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const ChatInterface = ({ children }) => {
-  return (
-    <div className="chat-interface">
-      {children}
-    </div>
-  );
-};
--- README.md
+++
@@ -1,2 +0,0 @@
-# PELICANAI
-Pelican is the perfect blend of powerful, intelligent, fast, accurate in a  local round AI
